32c0561cacb250bae5f7fc7c2944f0939ea8a133
Balaji Chandrababu <balaji.chandrababu@mirriad.com>
2015-08-28 15:30:08 +0100

    Add store app, view, template

---
+++ store/urls.py
@@ -0,0 +1,8 @@
+__author__ = 'balaji.chandrababu'
+
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.store, name='index')
+]
--- win_django_project/urls.py
+++ win_django_project/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'win_django_project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^store/', include('store.urls'), name='store'),
     url(r'^admin/', include(admin.site.urls)),
 ]
