8428dca90a1eea44279397ef486cec2ab91fbf42
Balaji Chandrababu <balaji.chandrababu@mirriad.com>
2015-10-08 15:26:16 +0100

    Adding initial base files for win django project

--- store/urls.py
+++ store/urls.py
@@ -1,8 +1,9 @@
 __author__ = 'balaji.chandrababu'
 
+
 from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.store, name='index')
+    url(r'^$', views.store, name='index'),
 ]
--- win_django_project/urls.py
+++ win_django_project/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
     url(r'^store/', include('store.urls'), name='store'),
     url(r'^admin/', include(admin.site.urls)),
+    # url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
